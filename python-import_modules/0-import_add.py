65d4d6d102455d910a460044ac33795395fd4167
Your Name <you@example.com>
2025-11-17 14:20:32 +0400

    salam

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-add = __import__("0-add").add
-add(1, 2)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
