25572ea42c140171939cb4901a6cdc1d0e169948
Your Name <you@example.com>
2025-11-17 15:28:04 +0400

    salam

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    count = len(argv)
+
+    if count == 0:
+        print("0 arguments.")
+    else:
+        if count == 1:
+            print("1 argument:")
+        else:
+            print(f"{count} arguments:")
+
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
