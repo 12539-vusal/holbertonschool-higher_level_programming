0b76bf1e8349239aa325d9af9dffd927ff301dd8
Your Name <you@example.com>
2025-11-27 17:55:26 +0400

    salam

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""salam"""
+
+from sys import argv
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+
+new_list = load_from_json_file('./add_item.json')
+new_list = []
+for i in range(1, len(argv)):
+    new_list.append(argv[i])
+save_to_json_file(new_list, './add_item.json')
