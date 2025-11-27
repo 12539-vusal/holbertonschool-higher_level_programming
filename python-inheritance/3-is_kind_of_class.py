f008044b9a5d3a2a9812799c50e9e26b75b3823e
Your Name <you@example.com>
2025-11-27 14:07:22 +0400

    salam

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" SALAM """
+
+
+def is_kind_of_class(obj, a_class):
+    """ SALAM """
+    if not isinstance(obj, a_class):
+        return False
+    else:
+        return True
