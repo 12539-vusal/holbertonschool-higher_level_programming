ac6457339d79ceb2e56707843734d96abb778278
Your Name <you@example.com>
2025-11-27 14:28:37 +0400

    salam

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ SALAM """
-    if issubclass(obj) and isinstance(obj, a_class):
+    if issubclass(obj) and isinstance(obj, a_class) and issubclass(type(obj), a_class):
         return True
     else:
         return False
