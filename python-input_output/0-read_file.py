e046cdc8395b3f35bf402c58645aa3dc7b18ef1a
Your Name <you@example.com>
2025-11-27 16:41:23 +0400

    salam

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """salam"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        for line in f.read():
+            print(line, end="")
