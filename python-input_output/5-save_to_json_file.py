f63c15b2746da74aa83568033ec604cb841fba0a
Your Name <you@example.com>
2025-11-27 17:39:59 +0400

    salam

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""salam"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """salam"""
+    with open(filename, mode="w", encoding='utf-8') as f:
+        json.dump(my_obj, f)
