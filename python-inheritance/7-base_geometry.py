36cdcfdce1aaa90f0465a97432a9058e31b46575
Your Name <you@example.com>
2025-11-27 15:50:11 +0400

    salam

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-""" salam """
+"""7. Integer validator"""
 
 
-class BaseGeometry():
-    """ salam """
+class BaseGeometry:
+    """Class BaseGeometry (based on 5-base_geometry.py)"""
+
     def area(self):
-        """ salam """
+        """Function that computes area (not implemented yet)"""
+
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        """ salam """
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        """
+        Function that validates name & value
+        Raises TypeError & ValueError if not int or positive resp.
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
