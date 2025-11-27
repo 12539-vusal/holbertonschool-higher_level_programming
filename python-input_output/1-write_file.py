fa8d3c980e52b5c9d5b13b07a99a84b3ce8bdaae
Your Name <you@example.com>
2025-11-27 16:58:17 +0400

    salam

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """salam"""
-    with open(filename, encoding="utf-8") as f
-    f.write(text)
+    with open(filename, encoding="utf-8") as f:
+        return f.write(text)
 
